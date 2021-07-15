2e681c1f9a10c6e39ee204100437134ca729c698
Your Name <you@example.com>
2021-07-15 12:24:50 +0800

    change

--- application/views/home_admin.php
+++ application/views/home_admin.php
@@ -297,7 +297,7 @@
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="navbar-toggler-icon"></span>
-                </button> <a class="navbar-brand" href="#">PIL-KB</a>
+                </button> <a class="navbar-brand" href="#">Votingku</a>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
                     <ul class="navbar-nav">
