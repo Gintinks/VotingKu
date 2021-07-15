c3b32720fbb89b54a271bb127e02e306d7060beb
Your Name <you@example.com>
2021-07-15 12:26:30 +0800

    test

--- application/views/adminLogin.php
+++ application/views/adminLogin.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="UTF-8" />
         <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
-        <title>PIL-KB</title>
+        <title>Votingku</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
         <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
@@ -22,7 +22,7 @@
             <!-- Codrops top bar -->
            
             <header>
-                <h1>ADMIN <span>PIL-KB</span></h1>
+                <h1>ADMIN <span>Votingku</span></h1>
 				
             </header>
             <section>				
