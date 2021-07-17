1520330a7a6bc941dc2ccf9a5d3ab6b4a64c03c0
NaufalHD <kadaite@hotmail.com>
2021-07-17 20:29:30 +0700

    tes coba

--- application/views/v_login2.php
+++ application/views/v_login2.php
@@ -21,7 +21,7 @@
     <body>
         
         <div class="container">
-            <!-- Codrops top bar -->
+            <!-- Codrops top bar -- testes dasofijaodsfjiafodsji>
            
             <header>
                 <h1>Login ke <span>PIL-KB</span></h1>
