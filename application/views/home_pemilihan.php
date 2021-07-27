a11e9ec60c6fbf1b8cfcb5fc0326ca00417be880
Your Name <you@example.com>
2021-07-27 10:59:45 +0800

    re-refix biarkan dulu kek gini

--- application/views/home_pemilihan.php
+++ application/views/home_pemilihan.php
@@ -38,34 +38,34 @@
 							<li class="nav-item">
 								<a class="nav-link" href="kritik">Kritik dan Saran</a>
 							</li>
+							<?php
+
+
+							if ($this->session->userdata('status') == "login") : ?>
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										<?php
+										echo $_SESSION["NIM"];
+										//echo implode("|",$this->session->userdata);
+										?>
+									</a>
+									<ul class="dropdown-menu dropdown-menu-dark overflow-visible" aria-labelledby="navbarDarkDropdownMenuLink">
+										<li><a class="dropdown-item" href="#">Logout</a></li>
+										<li><a class="dropdown-item" href="#">Another action</a></li>
+										<li><a class="dropdown-item" href="#">Something else here</a></li>
+									</ul>
+								</li>
+
+							<?php else : ?>
+								<li class="nav-item dropdown">
+									<a class="nav-link" href='login' id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										Login
+									</a>
+								</li>
+							<?php endif; ?>
+						</ul>
 					</div>
-					<ul class="navbar-nav">
-						<?php
-
-						// $sid = $this->session->nim;
-						if ($this->session->nim) : ?>
-							<li class="nav-item dropdown">
-								<a class="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-									<?php
-									echo $_SESSION["NIM"];
-									//echo implode("|",$this->session->userdata);
-									?>
-								</a>
-								<ul class="dropdown-menu dropdown-menu-dark overflow-visible" aria-labelledby="navbarDarkDropdownMenuLink">
-									<li><a class="dropdown-item" href="#">Logout</a></li>
-									<li><a class="dropdown-item" href="#">Another action</a></li>
-									<li><a class="dropdown-item" href="#">Something else here</a></li>
-								</ul>
-							</li>
-
-						<?php else : ?>
-							<li class="nav-item dropdown">
-								<a class="nav-link" href='login' id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-									Login
-								</a>
-							</li>
-						<?php endif; ?>
-					</ul>
+				
 
 				</nav>
 				<div class="jumbotron card card-block">
